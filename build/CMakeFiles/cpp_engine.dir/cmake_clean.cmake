c5652e196fd3934a546e4c2ffb19b5daf17a5a11
Harsh . <harsh.@Harshs-MacBook-Air.local>
2026-03-25 15:49:33 +1100

    file fixes

---
+++ build/CMakeFiles/cpp_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_engine.dir/main.cpp.o"
+  "CMakeFiles/cpp_engine.dir/main.cpp.o.d"
+  "cpp_engine"
+  "cpp_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CMakeFiles/videoEngine.dir/depend.make
+++
@@ -1,2 +0,0 @@
-﻿# Empty dependencies file for videoEngine.
-# This may be replaced when dependencies are built.
