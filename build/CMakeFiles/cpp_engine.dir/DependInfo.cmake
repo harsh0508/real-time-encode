958c916a2baeed3b2a1bfafad456aa6719a3af69
harsh0508 <Harsh.jvm@gmail.com>
2026-06-01 15:37:31 +1000

    updated gitignore -- revert

---
+++ build/CMakeFiles/cpp_engine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/harsh./Desktop/real-time-encode/cpp_engine/SM/shm_frame.cpp" "CMakeFiles/cpp_engine.dir/SM/shm_frame.cpp.o" "gcc" "CMakeFiles/cpp_engine.dir/SM/shm_frame.cpp.o.d"
+  "/Users/harsh./Desktop/real-time-encode/cpp_engine/encoder/encoder.cpp" "CMakeFiles/cpp_engine.dir/encoder/encoder.cpp.o" "gcc" "CMakeFiles/cpp_engine.dir/encoder/encoder.cpp.o.d"
+  "/Users/harsh./Desktop/real-time-encode/cpp_engine/main.cpp" "CMakeFiles/cpp_engine.dir/main.cpp.o" "gcc" "CMakeFiles/cpp_engine.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cpp_engine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_engine.dir/SM/shm_frame.cpp.o"
+  "CMakeFiles/cpp_engine.dir/SM/shm_frame.cpp.o.d"
+  "CMakeFiles/cpp_engine.dir/encoder/encoder.cpp.o"
+  "CMakeFiles/cpp_engine.dir/encoder/encoder.cpp.o.d"
+  "CMakeFiles/cpp_engine.dir/main.cpp.o"
+  "CMakeFiles/cpp_engine.dir/main.cpp.o.d"
+  "cpp_engine"
+  "cpp_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
